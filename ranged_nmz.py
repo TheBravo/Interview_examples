d7a370f4cfba470ae6681057ba4587f8e0590d7a
Jay Berns <jbeem30@gmail.com>
2023-03-06 07:38:25 -0500

    Update ranged_nmz.py

--- ranged_nmz.py
+++ ranged_nmz.py
@@ -5,7 +5,7 @@ pag.sleep(5)
 
 while True:
     while True:
-        if pag.pixelMatchesColor(22,456, (56,44,9)) == True: # Obstacle 2 (building jump)
+        if pag.pixelMatchesColor(22,456, (56,44,9)) == True: # (Reactivate pray, move mouse off screen)
             clickrandomlocation_quick(1845,793,1864,805)
             clickrandomlocation_quick(1845,793,1864,805)
             clickrandomlocation_nc(637,56,1607,927)        
